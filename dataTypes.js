1b5e8087f39fd29ac22bc93936b3ad432530058e
Aijaz ahmad Dar <110050419+daraijaz1122@users.noreply.github.com>
2023-11-09 17:37:12 +0000

    name changed to datatypes and added various datatypes

---
+++ dataTypes.js
@@ -0,0 +1,45 @@
+//DataTypes 
+//There are two types of data types (primitive and Non-Primitive)
+//Primitive DataTypes : these include(string,number,boolean,null,undefined,symbol,bigint)
+//Non-primitive/Reference Datatypes : these include(array,function,objects )
+//examples of primitive data types
+
+
+const Range = 100; 
+//console.log(typeof(Range));this will give us number
+
+
+const personName='Aijaz';
+//console.log(typeof(personName));//will return a string
+
+const RangeValue =100.5;
+//console.log(typeof(RangeValue));//will return number
+
+const isProgrammer = true;
+//console.log(typeof(isProgrammer));//will return boolean
+
+const outTemperature = null;
+//console.log(typeof(outTemperature));will return object
+
+let userEmail;
+//console.log(typeof(userEmail));//will return undefined
+
+const BigNumber = 123123123123123n;
+//console.log(typeof(BigNumber));//will return bigint
+
+//eg non-primitive data types
+
+const Cars=['bmw','audi',"toyota",'tesla'];
+//console.log(Cars,typeof(Cars));//this will returnn object
+
+const MyDetails ={
+    name:'aijaz',
+    age:26,
+    designation : 'software engineer'
+}
+//console.log(typeof(MyDetails));//this will return object
+
+const myFunction = function(){
+    console.log('this is a function body');
+}
+console.log(typeof(myFunction));//this will return function
